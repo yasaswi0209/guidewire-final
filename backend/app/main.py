6d65c59f360aa0d9c84dc3dc9e2b8817720340fd
Yasaswi-0209 <2300033733csemdie@gmail.com>
2026-04-05 00:59:31 +0530

    fix db engine ssl issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,10 @@ app = FastAPI()
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://guidewire-final.vercel.app",  # ✅ ADD THIS
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
